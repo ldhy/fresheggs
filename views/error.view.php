fe1aeef1b9711a7d0c4966707a293080b93ae78e
ldhy <laura.delahaye@gmail.com>
2018-05-26 18:54:45 +0200

    style error view

--- views/error.view.php
+++ views/error.view.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <title>Fresh Eggs</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-        <link rel="stylesheet" href="style/style.css"/>
+        <link rel="stylesheet" href="../style/style.css"/>
     </head>
 
     <body>
