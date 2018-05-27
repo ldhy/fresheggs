a7d8a0ba1d4e0dc2894759377932f5d3aa9e40f2
ldhy <laura.delahaye@gmail.com>
2018-05-27 17:24:13 +0200

    front recipes

--- views/recipes.view.php
+++ views/recipes.view.php
@@ -13,35 +13,26 @@
     }
 ?>
 
-
+<div class="container">
+	<div class="row ">
 <?php
 
 		foreach ($reqrecipes as $recipe)
-
     {
 ?>
-
-<div class="container">
-  <div class="row">
-    <div class="col-lg-4">
-      <div class="card" style="width: 18rem;">
+      <div class="card  col-md-6 col-lg-4" style="width: 18rem;">
         <div class="card-body">
           <h1 class="title-index"><?= $recipe['title']; ?></h1>
-            <h2 class="title-index"><?= $recipe['description']; ?></h2>
+            <p><?= $recipe['description']; ?></p>
             <a href="../pages/read.php?recipe_id=<?= $recipe['id'] ?>"><button type="button" class="btn btn-warning">Voir la recette</button></a>
         </div>
       </div>
-    </div>
-  </div>
-</div>
-
-
 <?php
-}
+    }
 ?>
 
-
-
+  </div>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 <?php require __DIR__.'/../layout/header.php'; ?>
