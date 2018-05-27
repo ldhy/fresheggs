5a204ac5bf4dcbb84faa61437bb00c709fde5427
ldhy <laura.delahaye@gmail.com>
2018-05-27 19:45:18 +0200

    edit

--- views/edit.view.php
+++ views/edit.view.php
@@ -1,7 +1,10 @@
 <?php ob_start(); ?>
 
-  <p><a href="recipes.php">Retour aux recettes</a></p>
-<h1>Modifier la recette</h1>
+<p><a href="recipes.php">Retour aux recettes</a></p>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-6">
+      <h1>Modifier la recette</h1>
 
 <?php
 if(isset($message))
@@ -9,7 +12,36 @@ if(isset($message))
   echo $message;
 }
 ?>
+        <form action="" method="post">
+          <div class="form-group">
+            <label>Titre</label>
+            <input type="text" class="form-control" value="<?= $readRecipe['title']; ?>" name="title" />
+          </div>
+          <div class="form-group">
+            <label>Description</label>
+            <input type="text" class="form-control" value="<?= $readRecipe['description']; ?>" name="description" />
+          </div>
+          <div class="form-group">
+            <label>Ingrédients</label>
+            <textarea type="text" class="form-control" rows="5" name="ingredients"><?= $readRecipe['ingredients']; ?></textarea>
+          </div>
+          <div class="form-group">
+            <label>La recette</label>
+            <textarea type="text" class="form-control" rows="7" name="content"><?= $readRecipe['content']; ?></textarea>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="type" id="oui" value="oui" checked>
+            <label class="form-check-label" for="oui">Oeufs extra frais</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="type" id="non" value="non" checked>
+            <label class="form-check-label" for="oui">Oeufs frais</label>
+          </div>
+          <input type="hidden" name="user_id"/>
+          <button type="submit" class="btn btn-warning" name="formupdate">Partager</button>
+        </form>
 
+<!--
       <form action="" method="post">
         <table>
           <tr>
@@ -47,6 +79,7 @@ if(isset($message))
         </table>
           <input type="submit" value="Modifier la recette" name="formupdate"/>
       </form>
+-->
 
 <?php $content = ob_get_clean(); ?>
 <?php require __DIR__.'/../layout/header.php'; ?>
