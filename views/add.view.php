82fcad3e3ac2dbaf14adf0cb775231d144369e00
ldhy <laura.delahaye@gmail.com>
2018-05-27 19:26:19 +0200

    read view

--- views/add.view.php
+++ views/add.view.php
@@ -1,15 +1,51 @@
 <?php ob_start(); ?>
 
-  <p><a href="recipes.php">Retour aux recettes</a></p>
-<h1>Partager une recette</h1>
 
+  <p><a href="recipes.php">Retour aux recettes</a></p>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <h1>Partager une recette</h1>
+
+<div class="red">
 <?php
 	if(isset($message))
 	{
 		echo $message;
 	}
 ?>
+</div>
 
+        <form action="" method="post">
+          <div class="form-group">
+            <label>Ajouter un titre</label>
+            <input type="text" class="form-control" name="title" />
+          </div>
+          <div class="form-group">
+            <label>Ajouter une petite description</label>
+            <input type="text" class="form-control" name="description" />
+          </div>
+          <div class="form-group">
+            <label>Ajouter les ingredients</label>
+            <textarea type="text" class="form-control" rows="5" name="ingredients"></textarea>
+          </div>
+          <div class="form-group">
+            <label>Ajouter la recette</label>
+            <textarea type="text" class="form-control" rows="5" name="content"></textarea>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="type" id="oui" value="oui" checked>
+            <label class="form-check-label" for="oui">Oeufs extra frais</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="type" id="non" value="non" checked>
+            <label class="form-check-label" for="oui">Oeufs frais</label>
+          </div>
+          <input type="hidden" name="user_id"/>
+          <button type="submit" class="btn btn-warning" name="formaddrecipes">Partager</button>
+        </form>
+
+<!--
   <form action="" method="post">
     <table>
       <tr>
@@ -48,7 +84,9 @@
   		<input type="hidden" name="user_id"/>
       <input type="submit" value="Partager" name="formaddrecipes"/>
   </form>
-
-
+-->
+  </div>
+    </div>
+      </div>
 <?php $content = ob_get_clean(); ?>
 <?php require __DIR__.'/../layout/header.php'; ?>
--- views/read.view.php
+++ views/read.view.php
@@ -2,23 +2,37 @@
 
 <p><a href="recipes.php">Voir les recettes</a></p>
 
-    <h1><?= $readRecipe['title']; ?></h1>
-      <p><?= $readRecipe['description']; ?></p>
-      <p><?= $readRecipe['ingredients']; ?></p>
-      <p><?= $readRecipe['content']; ?></p>
+<div class="container">
+  <div class="row">
+      <div class="card col-lg-12" style="width: 18rem;">
+        <div class="card-body">
+          <h1 class="title-index"><?= $readRecipe['title']; ?></h1>
+            <p><?= $recipe['description']; ?></p>
+            <p><?= $readRecipe['description']; ?></p>
+            <p><?= $readRecipe['ingredients']; ?></p>
+            <p><?= $readRecipe['content']; ?></p>
+        </div>
+
+      <!--
+      <h1><?= $readRecipe['title']; ?></h1>
+        <p><?= $readRecipe['description']; ?></p>
+        <p><?= $readRecipe['ingredients']; ?></p>
+        <p><?= $readRecipe['content']; ?></p>
+      -->
 
 <?php
 if($_SESSION['id'] != $readRecipe['user_id']) {
 ?>
-      <p><a href="addFav.php?favorites_id=<?= $readRecipe['id']; ?>">Ajouter à mes favoris</a></p>
+      <p><a href="addFav.php?favorites_id=<?= $readRecipe['id']; ?>"><button type="button" class="btn btn-warning">Ajouter à mes favoris</button></a></p>
+
 <?php
 }
 
 if($_SESSION['id'] == $readRecipe['user_id']) {
 ?>
     <h4>Ceci est ma recette :) </h4>
-    <a href="edit.php?recipe_id=<?= $readRecipe['id'] ?>"><?= 'Modifier'?></a>
-    <a href="delete.php?recipe_id=<?= $readRecipe['id'] ?>"><?= 'Supprimer'?></a>
+    <p><a href="edit.php?recipe_id=<?= $readRecipe['id'] ?>"><button type="button" class="btn btn-warning">Modifer</button></a>
+    <a href="delete.php?recipe_id=<?= $readRecipe['id'] ?>"><button type="button" class="btn btn-warning">Supprimer</button></a></p>
 <?php
 }
 ?>
@@ -30,6 +44,8 @@ if(isset($message))
   echo $message;
 }
 ?>
-
+</div>
+  </div>
+    </div>
 <?php $content = ob_get_clean(); ?>
 <?php require __DIR__.'/../layout/header.php'; ?>
